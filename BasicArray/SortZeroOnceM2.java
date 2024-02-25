40214c3684c88413961be3f69caa363fe3d9376c
PrabhatSinghhh <prabhatsingh9006@gmail.com>
2024-02-25 18:22:28 +0530

    change file name

--- BasicArray/SortZeroOnceM2.java
+++ BasicArray/SortZeroOnceM2.java
@@ -3,21 +3,20 @@ package BasicArray;
 public class SortZeroOnceM2 {
 
         public static void main(String[] args) {
-            int[] arr = {0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0};
+
+            int[] arr = {0,1,1,1,1,0,0,0,1,1,0};
             int n = arr.length;
             int i = 0, j = n - 1;
             while (i < j) {
-                if (arr[i] == 0) {
-                    i++;
-                } else if (arr[j] == 1) {
-                    j--;
-                } else if (arr[i] == 1 && arr[j] == 0) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+                if (arr[i] == 0) i++;
+                else if (arr[j] == 1) j--;
+                else if (arr[i] == 1 && arr[j] == 0) {
+                    arr[i] = 0;
+                    arr[j] = 1;
                     i++;
                     j--;
                 }
+            }
 
                 for (int ele : arr) {
                     System.out.print(ele + " ");
@@ -25,4 +24,4 @@ public class SortZeroOnceM2 {
 
             }
         }
-    }
+
